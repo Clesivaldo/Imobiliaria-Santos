f6a0219c1d59525c63ed8a33aaee9478b4293f81
Clesivaldo Santos <71999512+Clesivaldo@users.noreply.github.com>
2023-05-16 23:15:29 -0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,16 @@
+
+const swiper = new Swiper(".heroslider", {
+	spaceBetween: 30,
+	slidesPerView: 1,
+	loop: true,
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	effect: "fade",
+	navigation: {
+		nextEl: ".swiper-button-next",
+		nextEl: ".swiper-button-prev",
+	},
+});
+
